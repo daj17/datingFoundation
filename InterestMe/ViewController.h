e0ecec96e2795590163416fcead43321ae3a5fe9
Portanos <Portanos@harvardsucks>
2016-05-26 20:35:18 -0700

    Initial Commit

---
+++ InterestMe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InterestMe
+//
+//  Created by Portanos on 5/26/16.
+//  Copyright © 2016 ChimpchatLLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ InterestMe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  InterestMe
+//
+//  Created by Portanos on 5/26/16.
+//  Copyright © 2016 ChimpchatLLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
